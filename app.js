d1e829f15c616375327a7e7d2b231444ae267e81
Tom Whitcomb <twhitcomb16@gmail.com>
2016-04-10 16:22:46 -0400

    made fn getTweets which makes API call

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
 $(document).ready(function(){
 	var container = $('.container')
-	var pageTweetsList = []
-	var requestURI = '1.1/search/tweets.json?q='
+	var currTweetsList = []
+	//DONT THINK I NEED THE BELOW LINE
+	//var requestURI = '1.1/search/tweets.json?q='
 	//var fireb = new Firebase("https://ga-final-project.firebaseio.com")
+	var repeatTweets = 0
 
 	$('.find-tweets-button').on('click',function(e){
 		e.preventDefault()
+		//add code for clearing all previous tweets
+		
+		//I dont know thatI need these variables
+		// var tweetGetCount = 15
+		// var resultType = 'popular'
 
-		inputVal = $('.search-box').val()
-		OAuth.initialize('XFW3J69pI4WCYLZsRuDEYhcSFFU')
-		OAuth.popup('twitter').done(function(result) {
-	    //console.log(result)
-	    result.get(requestURI + inputVal + '&result_type=mixed&count=20').done(function(resp){
-	    	console.log(resp)
-	    	resp.statuses.forEach(tweetCheck(obj))
-	    })
-		})	
+		inputKeyword = $('.search-box').val()
 		$('.search-box').val('')
+		getTweets(inputKeyword, 'popular', 15)
 	})
 
-	function tweetCheck(obj){
-		var id = obj.id_str
-		pageTweetsList.forEach(function(twtId){
-			if(id === twtId){
-				//return False
-			} else {
-				createTweetBox(obj)
-				pageTweetsList.push(id)
-			}
+	function getTweets(keyword, resultType, tweetNum){
+	    //console.log(result)
+		OAuth.initialize('XFW3J69pI4WCYLZsRuDEYhcSFFU')
+		OAuth.popup('twitter').done(function(result) {
+		    result.get('1.1/search/tweets.json?q=' + keyword + '&result_type=' + resultType + '&count=' + tweetNum).done(function(resp){
+		    	console.log(resp)
+		    	resp.statuses.forEach(function(obj){
+		    		tweetCheck(obj)
+		    	})
+		    })
 		})
 	}
 
-	function createTweetBox(obj){
 
+	function tweetCheck(obj){
+		//console.log(obj.id_str)
+		var id = obj.id_str
+		var exists = true
+		
+		for(i=0; i<currTweetsList.length;i++){
+			if (currTweetsList[i] === id) {
+				exists = false
+			}
+		}
+		if(exists){
+			currTweetsList.push(id)
+			createTweetBox(obj)
+		} else {
+			repeatTweets += 1 
+			//NEED TO FIGURE OUT HOW TO CALL GET TWEETS WHEN THIS IS TRUE
+		}
+	}
+	
+
+	function createTweetBox(obj){
+		console.log('made it to create Tweet Box')
 		//make next line .addClass('box') to add to actual box not just line
     	var $tweetBox = $('<div></div>').addClass('twt-box')
     	var tweetText = obj.text
@@ -44,6 +66,8 @@ $(document).ready(function(){
 	}
 
 	
-
+	$(window).scroll(function(e){
+		console.log(e)
+	})
 
 })
