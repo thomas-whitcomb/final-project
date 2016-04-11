bf73ba0358b4782030bc05a445b37da740968b12
Tom Whitcomb <twhitcomb16@gmail.com>
2016-04-10 23:52:30 -0400

    added font design to html and cleaned up js - still need to comment

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-	var container = $('.container')
 	var tweetList = $('.tweet-list')
 	var templateHtml = $('#row').html();
   	var compiledTemplate = Handlebars.compile(templateHtml);
 	var currTweetsList = []
-	//DONT THINK I NEED THE BELOW LINE
-	//var requestURI = '1.1/search/tweets.json?q='
 	//var fireb = new Firebase("https://ga-final-project.firebaseio.com")
 	var repeatTweets = 0
 	var inputKeyword = ''
@@ -14,17 +11,12 @@ $(document).ready(function(){
 		e.preventDefault()
 		//add code for clearing all previous tweets
 		
-		//I dont know thatI need these variables
-		// var tweetGetCount = 15
-		// var resultType = 'popular'
-
 		inputKeyword = $('.search-box').val()
 		$('.search-box').val('')
 		getTweets(inputKeyword, 'popular', 15)
 	})
 
 	function getTweets(keyword, resultType, tweetNum){
-	    //console.log(result)
 		OAuth.initialize('XFW3J69pI4WCYLZsRuDEYhcSFFU')
 		OAuth.popup('twitter').done(function(result) {
 		    result.get('1.1/search/tweets.json?q=' + keyword + '&result_type=' + resultType + '&count=' + tweetNum).done(function(resp){
@@ -38,7 +30,6 @@ $(document).ready(function(){
 
 
 	function tweetCheck(obj){
-		//console.log(obj.id_str)
 		var id = obj.id_str
 		var exists = true
 		
@@ -52,7 +43,7 @@ $(document).ready(function(){
 			createTweetBox(obj)
 		} else {
 			repeatTweets += 1 
-			//NEED TO FIGURE OUT HOW TO CALL GET TWEETS WHEN THIS IS TRUE
+			//will use this code when I make fn for when user goes to bottom of page and need to reload tweets
 		}
 	}
 	
@@ -61,29 +52,19 @@ $(document).ready(function(){
     	var tweetText = obj.text
     	var twitterHandle = '@' + obj.user.screen_name
     	var profileName = obj.user.name
+    	var profileURL = obj.user.url
+    	var profImgURL = obj.user.profile_background_image_url
 
     	var row = compiledTemplate({
     		name: profileName,
    			screenName: twitterHandle,
-   			twtText: tweetText
+   			twtText: tweetText,
+   			profURL: profileURL,
+   			profImg: profImgURL
     	})
-
     	tweetList.append(row)
-
-    	// //$tweetBox.css({backgroundColor: 'red'})
-  	  // 	$tweetBox.html('@' + twitterHandle + '\n' + tweetText)
-  	  // 	container.append($tweetBox)
 	}
 
-	function makeRow(newText){
-  
-  var row = compiledTemplate({
-      todoThing: newText
-    });
-  tweetList.append(row);
-};
-
-	
 	// $(window).scroll(function(e){
 	// 	console.log(e)
 	// 	//below from internet - still need to figure out how/when to call getTweets
